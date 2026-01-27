62cf47f13ecbe7d3155b365a4867bd59bc6b1ac2
Abdullah Shami <az912706@gmail.com>
2026-01-27 18:43:08 +0500

    Done

---
+++ First code/functions.py
@@ -0,0 +1,24 @@
+'name = input("Whats Your name:").strip().title()'
+'''name = name.strip()
+name = name.title()'''
+'name = name.strip().title()'
+'split functions here we are spliting the name into two substrings'
+
+'''fist , middle , last = name.split(" ")
+
+print(f"Hello, {middle}")'''
+
+''''first = float(input("Enter First Number:"))
+second = float(input("Enter Second Number:"))
+sign = input("Enter the operation like + , - , * , /")
+
+if sign == '+':
+    print(first+second)
+elif sign == '-':
+    print(first-second)
+elif sign == '*':
+    print(first*second)
+elif sign == '/':
+    print(first/second)
+else:
+    print("Invalid Choice")'''''
---
+++ First code/main.py
@@ -0,0 +1,15 @@
+'''def main():
+    name = input("What's your name? ")
+    match name:
+        case "Abdullah":
+            print("199")
+        case _:
+            print("Who?")
+main()'''
+
+def main():
+    i = 1
+    while i <= 3:
+        print("meow 1")
+        i = i + 1
+main() 
