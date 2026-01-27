e8f7b004344b2a307b31c1500f52ffde0d6f6213
Abdullah Shami <az912706@gmail.com>
2026-01-27 20:35:26 +0500

    updated

--- First code/main.py
+++ First code/main.py
@@ -1,21 +1,29 @@
-'''def main():
+def main():
     name = input("What's your name? ")
     match name:
         case "Abdullah":
             print("199")
         case _:
             print("Who?")
-main()'''
+main()
 
-'''def main():
+def main():
     i = 1
     while i <= 3:
         print("meow")
         i = i + 1
-main()''' 
-
+main()
 
 def main():
     for _ in range(3):
         print("Meow" , _)
 main()
+n = int(input("What's n?"))
+print("Meow\n" * n , end="")
+
+students = ["Abdullah" , "Rafay" ,"Raheem" , 6]
+for i in students:
+    print("[",i, sep="," ,end="" )
+print()
+for i in [1,4,6]:
+    print(i)
