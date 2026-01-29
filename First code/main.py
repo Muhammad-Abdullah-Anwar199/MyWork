c4215a272d2ef0a8be31736f96768fb6652be74f
Abdullah Shami <az912706@gmail.com>
2026-01-29 20:57:20 +0500

    Some daily contribution

--- First code/main.py
+++ First code/main.py
@@ -27,3 +27,56 @@ for i in students:
 print()
 for i in [1,4,6]:
     print(i)
+
+
+def main():
+    family = {
+        "Abdullah" : "Shami House" ,
+        "ALi " : "Goraya's House"
+}
+    for i in family:
+        print(i , family[i] , sep="->")
+main()
+
+
+students = (
+{"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+{"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+{"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell terrier"},
+{"name": "Draco", "house": "Slytherin", "patronus": None}
+)
+
+for student in students:
+    print(student["name"] , student["house"] , student["patronus"])
+
+
+print("#" * 3 , end="")
+
+def main():
+    col = print_column(0)
+    rows = print_rows(0)
+
+    print(rows)
+    print(col)
+
+    print("*" * 0)
+    print("*" * 0)
+
+    print_square(3)
+
+def print_column(width):
+    return "*" * width
+
+def print_rows(height):
+    return "*" * height
+
+def print_square(size):
+    #for each row in square.
+    for _ in range(size):
+        #for each brick in rows
+        for _ in range(size):
+            #print bricks
+            print("#" , end="")
+        print() #this is for simple line switch
+
+main()
