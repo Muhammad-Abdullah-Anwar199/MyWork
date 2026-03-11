63127e27374389e25a952bea49434c6c5ca6c90c
Abdullah Shami <az912706@gmail.com>
2026-03-11 15:41:55 +0500

    dddd

--- Regex/validate.py
+++ Regex/validate.py
@@ -1,6 +1,8 @@
 email = input("What's your email? ").strip()
 
-if "@" in email and "." in email:
+username , domain = email.split("@")
+print(username,domain)
+if username and "." in domain:
     print("Valid")
 else:
     print("Invalid")
