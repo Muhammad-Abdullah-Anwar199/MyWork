7d66674630f0e8415ea0273a054690d77f163582
Abdullah Shami <az912706@gmail.com>
2026-03-13 15:02:31 +0500

    ddddd

--- OOP/basic.py
+++ OOP/basic.py
@@ -3,11 +3,15 @@ class Car:
         self.__brand = brand
         self.__model = model
         self.__year = year
-
+    #this is only for returing string that why it named (str)
     def __str__(self):
         return f"Brand: {self.brand}, Model: {self.model}, Year: {self.year}"
+    #function = decorator(function)
+    @property
+    def brand(self):
+        return self.__brand
 
 
 if __name__ == "__main__":
     c1 = Car("Toyota", "Corolla", 2022)
-    print(c1._Car__model)
+    print(c1.brand)
